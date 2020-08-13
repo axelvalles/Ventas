31d6867acd9996a7677934e06cc74b5ee2b3ce08
Brayan Forero <=brayanroswll2802@gmail.com>
2020-08-13 14:48:08 -0400

    Cambia formato a 2 espacios de indentacion

--- index..php
+++ index..php
@@ -2,60 +2,57 @@
 <html lang="es">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-    <title>VentasDimeca</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <title>VentasDimeca</title>
 </head>
 
 <body>
+  <div class="container">
+    <div class="row">
+      <div class="col d-flex justify-content-center mt-5">
+        <div class="card text-center w-50">
+          <div class="card-header">
+            <h1 class="d-block">Acceso al sistema</h1>
+          </div>
+          <div class="card-body">
 
+            <form id="form">
+              <div class="form-group">
+                <label for="user">Usuario</label>
+                <input type="text" class="form-control" id="user" name="user" aria-describedby="userHelp" placeholder="Ingrese usuario">
+                <small id="userHelp" class="form-text text-muted"></small>
+              </div>
+              <div class="form-group">
+                <label for="password">Contraseña</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Ingrese contraseña">
+              </div>
+              <button type="submit" class="btn btn-primary btn-block mb-2">Ingresar</button>
+            </form>
 
-    <div class="container">
-        <div class="row">
-            <div class="col d-flex justify-content-center mt-5">
-                <div class="card text-center w-50">
-                    <div class="card-header">
-                        <h1 class="d-block">Acceso al sistema</h1>
-                    </div>
-                    <div class="card-body">
-
-                        <form id="form">
-                            <div class="form-group">
-                                <label for="user">Usuario</label>
-                                <input type="text" class="form-control" id="user" name="user" aria-describedby="userHelp" placeholder="Ingrese usuario">
-                                <small id="userHelp" class="form-text text-muted"></small>
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Contraseña</label>
-                                <input type="password" class="form-control" id="password" name="password" placeholder="Ingrese contraseña">
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-block mb-2">Ingresar</button>
-                        </form>
-
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 
 
 
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 
-    <script src="frontEnd/scripts/login.js"></script>
+  <script src="frontEnd/scripts/login.js"></script>
 </body>
 
 </html>
