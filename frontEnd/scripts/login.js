5f2f02cfb150316140e7618d12d1f8760d4b7070
Brayan Forero <=brayanroswll2802@gmail.com>
2020-08-13 14:50:40 -0400

    Formatea y cambia titulo de la alerta

--- frontEnd/scripts/login.js
+++ frontEnd/scripts/login.js
@@ -1,69 +1,62 @@
+const form = document.getElementById("form");
+const user = document.getElementById("user").value;
+const pass = document.getElementById("password").value;
 
-const form = document.getElementById('form')
-const user  =  document.getElementById('user').value
-const pass  =  document.getElementById('password').value
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("me diste un click");
 
+  formData = new FormData(form);
 
-form.addEventListener('submit',(e) =>{
-    e.preventDefault();
-    console.log('me diste un click');
+  console.log(user);
+  console.log(pass);
+  get();
+});
 
-    formData = new FormData(form);
+let get = () => {
+  fetch("backEnd/Functions/Login/VerifyUsers.php", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data === "ok") {
+        Swal.fire({
+          position: "Center",
+          icon: "success",
+          title: "Bienvenido",
+          showConfirmButton: false,
+          timer: 1000,
+        });
 
-    console.log(user);
-    console.log(pass);
-    get()
-})
+        setTimeout(function () {
+          window.location = "/dimeca/frontend/views/main.php";
+        }, 1500);
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "El usuario no existe :(",
+        });
+      }
+    });
+};
 
-
-let get = ()=>{
-    fetch('backEnd/Functions/Login/VerifyUsers.php',{
-        method:'POST',
-        body:formData
-    })
-    .then(res=>res.json())
-    .then(data=>{
-        if(data === 'ok'){
-            Swal.fire({
-                position: 'Center',
-                icon: 'success',
-                title: 'Bienvenido',
-                showConfirmButton: false,
-                timer: 1000
-            })
-            
-            setTimeout(function(){
-                window.location = '/dimeca/frontend/views/main.php'
-            },1500);
-        }else{
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'El usuario no existe :(',
-            })
-        }
-    })
-}
-
-
-let getData =  async (url,method,data) =>{
-
-    const location = url;
-    const settings = {
-                        method: method,
-                        headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json',
-                        },
-                        body:data
-                    }
-    try {
-        let res = await fetch(location,settings)
-        let data = await res.json()
-        console.log(data);
-
-    } catch (e) {
-        console.log(e);
-    }
-
-    }
+let getData = async (url, method, data) => {
+  const location = url;
+  const settings = {
+    method: method,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: data,
+  };
+  try {
+    let res = await fetch(location, settings);
+    let data = await res.json();
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+};
